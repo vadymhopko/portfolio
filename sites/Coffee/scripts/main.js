086efbe2fea26f88e7dc5ff8207ce6a58e15b777
vadymgopko <gopko.vadim@gmail.com>
2022-04-30 17:30:25 +0300

    coffe chages counters

--- sites/Coffee/scripts/main.js
+++ sites/Coffee/scripts/main.js
@@ -20,3 +20,29 @@ for (let i = 0; i < galleryPic.length; i++) {
     overlay.classList.toggle("show-overlay");
   });
 }
+
+// Добавляем прослушку на всем окне
+window.addEventListener("click", function (event) {
+  let counter;
+
+  if (
+    event.target.dataset.action === "plus" ||
+    event.target.dataset.action === "minus"
+  ) {
+    const counterWrapper = event.target.closest(".offer-card-amount");
+
+    counter = counterWrapper.querySelector("[data-counter]");
+  }
+
+  if (event.target.dataset.action === "plus") {
+    if (parseInt(counter.innerText) < 20) {
+      counter.innerText = ++counter.innerText;
+    }
+  }
+
+  if (event.target.dataset.action === "minus") {
+    if (parseInt(counter.innerText) > 1) {
+      counter.innerText = --counter.innerText;
+    }
+  }
+});
